285e8418caa478479273725526e62b8485ccaef4
Oğuzhan Ahıskalı <oguzhan@ahiskali.org>
2022-11-30 11:58:55 +0300

    asd

--- conf.php
+++ conf.php
@@ -1,10 +1,10 @@
 <?php
 
-$mysqli = new mysqli("127.0.0.1", "v2ep_usr", "Mzw?s151F4#3daw1", "v2ep_db");
+$mysqli = new mysqli("127.0.0.1", "v2ep_usr", "Mzw?s151F4#3daw1", "db_mektapp");
 $mysqli->set_charset("utf8");
 
 
-$link = new mysqli("127.0.0.1", "v2ep_usr", "Mzw?s151F4#3daw1", "v2ep_db");
+$link = new mysqli("127.0.0.1", "v2ep_usr", "Mzw?s151F4#3daw1", "db_mektapp");
 
 
 if(mysqli_connect_error()) //Eğer hata varsa yazdırıyoruz
@@ -16,9 +16,9 @@ if(mysqli_connect_error()) //Eğer hata varsa yazdırıyoruz
 
 $link->set_charset("utf8");
 
-$con=mysqli_connect('127.0.0.1','v2ep_usr','Mzw?s151F4#3daw1','v2ep_db')or die("connection failed".mysqli_errno());
+$con=mysqli_connect('127.0.0.1','v2ep_usr','Mzw?s151F4#3daw1','db_mektapp')or die("connection failed".mysqli_errno());
 $con->query("SET CHARACTER SET utf8");
 
 
-$db = new PDO("mysql:host=127.0.0.1;dbname=v2ep_db;charset=utf8", "v2ep_usr", "Mzw?s151F4#3daw1");
+$db = new PDO("mysql:host=127.0.0.1;dbname=db_mektapp;charset=utf8", "v2ep_usr", "Mzw?s151F4#3daw1");
 ?>
