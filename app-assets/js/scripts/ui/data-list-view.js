a96c4f9ccaa2b6331fcf4473f4d963cfbd66da3e
Oğuzhan Ahıskalı <oguzhan@ahiskali.org>
2022-12-02 21:27:18 +0300

    asd

--- app-assets/js/scripts/ui/data-list-view.js
+++ app-assets/js/scripts/ui/data-list-view.js
@@ -137,25 +137,6 @@ $(document).ready(function() {
     e.stopPropagation();
     $(this).closest('td').parent('tr').fadeOut();
   });
-
-  // dropzone init
-  Dropzone.options.dataListUpload = {
-    complete: function(files) {
-      var _this = this
-      // checks files in class dropzone and remove that files
-      $(".hide-data-sidebar, .cancel-data-btn, .actions .dt-buttons").on(
-        "click",
-        function() {
-          $(".dropzone")[0].dropzone.files.forEach(function(file) {
-            file.previewElement.remove()
-          })
-          $(".dropzone").removeClass("dz-started")
-        }
-      )
-    }
-  }
-  Dropzone.options.dataListUpload.complete()
-
   // mac chrome checkbox fix
   if (navigator.userAgent.indexOf("Mac OS X") != -1) {
     $(".dt-checkboxes-cell input, .dt-checkboxes").addClass("mac-checkbox")
