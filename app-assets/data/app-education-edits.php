ba22f41855c95aa53e1e6fb2765959500eb90991
Oğuzhan Ahıskalı <oguzhan@ahiskali.org>
2022-12-02 21:40:56 +0300

    Update app-education-edits.php

--- app-assets/data/app-education-edits.php
+++ app-assets/data/app-education-edits.php
@@ -9,31 +9,21 @@ if ($result = $link->query("SELECT * FROM tbl_egitim_grup_join WHERE GRUP_ID = '
 
     while($row = $result->fetch_array(MYSQLI_ASSOC)) {
 			$joinInfo = null;
-			$joinType = null;
 			$color = null;
-			if($row['JOIN_TYPE']=='sirket'){
-					$joinType = 'Şirket';
-					$grup = $db->query("SELECT * FROM tbl_company WHERE ID = {$row['JOIN_ID']}")->fetch(PDO::FETCH_ASSOC);
+			$grup = $db->query("SELECT * FROM tbl_company WHERE ID = {$row['JOIN_ID']}")->fetch(PDO::FETCH_ASSOC);
+
+			if(      isset($grup['companyName'])){ $joinInfo = $grup['companyName'];
+			}else if(isset($grup['companyTableName'])){ $joinInfo = $grup['companyTableName'];
+			}else if(isset($grup['competentFirstname'])){   $joinInfo = $grup['competentFirstname'].' '.$grup['competentLastname'];
+			}else{ $joinInfo = 'null';
 	
-					if(      isset($grup['companyName'])){ $joinInfo = $grup['companyName'];
-					}else if(isset($grup['companyTableName'])){ $joinInfo = $grup['companyTableName'];
-					}else if(isset($grup['competentFirstname'])){   $joinInfo = $grup['competentFirstname'].' '.$grup['competentLastname'];
-					}else{ $joinInfo = 'null';
-	
-					}
-	
-			}else if($row['JOIN_TYPE']=='vatandas'){
-					$joinType = 'Vatandaş';
-					$grup = $db->query("SELECT * FROM tbl_musteri_kimlik WHERE ID = {$row['JOIN_ID']}")->fetch(PDO::FETCH_ASSOC);
-					$joinInfo = $grup['ADI'].' '.$grup['SOYADI'];
-			}
+				
 			if($row['INVITATION_STATUS']==1){ $invitationStatus = true; $colorA = 'chip-success'; }else{ $invitationStatus = false; $colorA = 'chip-danger'; }
 			if($row['JOIN_STATUS']==1){ $JoinStatus = true; $colorB = 'chip-success'; }else{ $JoinStatus = false; $colorB = 'chip-danger'; }
 			if($row['STATUS']==1){ $Status = 'Aktif'; $colorC = 'chip-success'; }else{ $Status = false; $colorC = 'chip-danger'; }
 	
 		$myArray[] = [
 			'ID'	=> intval($row['ID']),
-			'Education'=> $joinType,
 			'EducationID'=> $joinInfo,
 			'Group'	=> $joinInfo,
 			'GroupID'	=> $sessionID,
