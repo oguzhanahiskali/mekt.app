b6146ebee994449c294fc6893a69897804fdfece
Oğuzhan Ahıskalı <oguzhan@ahiskali.org>
2022-12-02 21:45:53 +0300

    Create ajaxQEducations.php

---
+++ api/ajaxQEducations.php
@@ -0,0 +1,32 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT'].'/header-top.php';
+
+$json = [];
+
+if(!empty($_GET['id'])){
+
+   $id = $_GET['id'];
+
+   $sql = "SELECT * FROM tbl_egitim_grup WHERE ID = '$id'";
+   $result = $mysqli->query($sql);
+
+   while($row = $result->fetch_assoc()){
+      $json = [
+         'ID'      =>  $row['ID'],
+         'GRUP' =>  $row['GRUP_ADI'],
+         'EDU_ID' =>  $row['EGITIM_ID'],
+         'START'=>  $row['START'],
+         'FINISH' =>  $row['FINISH'],
+         'STATUS' =>  $row['STATUS'],
+         'DT'     =>  $row['DT']
+      ];
+   }
+
+}else{
+   $json[]=[
+      'Results'   => 'Bad request!'
+   ];
+}
+      
+      echo json_encode($json);
+?>
